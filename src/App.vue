f9f52389e72857977a7539fd37945bd672632750
Fanrr <2514509399@qq.com>
2021-01-05 14:25:07 +0800

    v-model

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{ user }}
+    <form @submit.prevent="onsubmit">
+      <label>
+        <span>用户名</span>
+        <!-- <input type="text" v-model="user.username" /> 与下面一段等价-->
+        <!-- <input
+          type="text"
+          :value="user.username"
+          @input="user.username = $event.target.value"
+        /> -->
+        <!-- <MyInput v-model="user.username" /> 与下面一行等价-->
+        <MyInput :value="user.username" @input="user.username = $event" />
+      </label>
+      <label>
+        <span>密码</span>
+        <input type="password" v-model="user.password" />
+      </label>
+      <button type="submit">登陆</button>
+    </form>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import MyInput from "./MyInput.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: { MyInput },
+  name: "App",
+  data() {
+    return {
+      user: {
+        username: "",
+        password: "",
+      },
+      x: [],
+    };
+  },
+  methods: {
+    onsubmit() {
+      console.log(this.user.username, this.user.password);
+    },
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/MyInput.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <input :value="value" @input="onInput" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyInput",
+  props: {
+    value: {
+      type: String,
+    },
+  },
+  methods: {
+    onInput(e) {
+      const value = e.target.value;
+      this.$emit("input", value); //$emit相当于eventBus将数据放到$event
+    },
+  },
+};
+</script>
